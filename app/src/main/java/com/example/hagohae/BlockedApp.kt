c1a925a4a54f5be238418d406bf2705f7772989d
songgyubin <thd0427__@naver.com>
2024-09-20 20:05:38 +0900

    특정 앱 차단 테스트 완료

---
+++ app/src/main/java/com/example/hagohae/BlockedApp.kt
@@ -0,0 +1,12 @@
+package com.example.hagohae
+
+/**
+ *
+ *
+ * @author   Gyub
+ * @created  2024/09/20
+ */
+data class BlockedApp(
+    val packageName: String,
+    val unblockTime: Long // 차단 해제 시간 (밀리초)
+)
