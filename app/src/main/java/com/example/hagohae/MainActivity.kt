5c7a3d4fb6e55f337b8dbf1ece1ca29fee1eeafa
songgyubin <thd0427__@naver.com>
2024-09-24 14:42:26 +0900

    feat: BpkTheme 적용

--- app/src/main/java/com/example/hagohae/MainActivity.kt
+++ app/src/main/java/com/example/hagohae/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
+import com.example.hagohae.home.ui.HomeScreen
 import com.example.hagohae.ui.theme.HagohaeTheme
+import net.skyscanner.backpack.compose.theme.BpkTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -13,8 +16,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            HagohaeTheme {
-
+            BpkTheme {
+                Scaffold(
+                    contentColor = BpkTheme.colors.surfaceDefault
+                ) { innerPadding ->
+                    HomeScreen(innerPadding)
+                }
             }
         }
     }
