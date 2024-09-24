bd0a8a0b28aa9b6bfe35d4071d822e44f80882c9
songgyubin <thd0427__@naver.com>
2024-09-24 18:08:08 +0900

    feat: 미션 상세 화면 Top Bar 추가

---
+++ app/src/main/java/com/example/hagohae/mission/MissionDetailScreen.kt
@@ -0,0 +1,17 @@
+package com.example.hagohae.mission
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+/**
+ * 미션 상세 화면
+ *
+ * @author   Gyub
+ * @created  2024/09/24
+ */
+@Composable
+fun MissionDetailScreen(
+    innerPadding: PaddingValues
+) {
+
+}
---
+++ app/src/main/java/com/example/hagohae/mission/MissionDetailTopAppBar.kt
@@ -0,0 +1,41 @@
+package com.example.hagohae.mission
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.gyub.hagohae.R
+import net.skyscanner.backpack.compose.navigationbar.BpkTopNavBar
+import net.skyscanner.backpack.compose.navigationbar.NavIcon
+import net.skyscanner.backpack.compose.theme.BpkTheme
+
+/**
+ * 미션 상세 탑 바
+ *
+ * @author   Gyub
+ * @created  2024/09/29
+ */
+@Composable
+fun MissionDetailTopAppBar(
+    modifier: Modifier = Modifier,
+    isEdit: Boolean,
+) {
+    BpkTopNavBar(
+        navIcon = NavIcon.Back(
+            contentDescription = "Back",
+            onClick = {}
+        ),
+        title = if (isEdit) stringResource(R.string.mission_edit) else stringResource(R.string.mission_detail),
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+private fun MissionDetailTopAppBarPreview() {
+    BpkTheme {
+        MissionDetailTopAppBar(
+            isEdit = false
+        )
+    }
+}
