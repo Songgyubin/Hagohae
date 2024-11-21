1bc63ffc83688dcc04522d8c4cc16c8c3ad171cc
songgyubin <thd0427__@naver.com>
2024-11-21 21:20:40 +0900

    feat: 앱 차단 섹션 추가

--- app/src/main/java/com/gyub/hagohae/mission/MissionDetailRoute.kt
+++ app/src/main/java/com/gyub/hagohae/mission/MissionDetailRoute.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -27,14 +28,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.gyub.hagohae.R
 import net.skyscanner.backpack.compose.card.BpkCard
 import net.skyscanner.backpack.compose.card.BpkCardCorner
 import net.skyscanner.backpack.compose.theme.BpkTheme
+import com.example.resource.R as ResourceR
 
 /**
  * 미션 상세 화면
@@ -97,6 +100,12 @@ fun MissionBlockingApp() {
             Text(
                 modifier = Modifier.weight(1f),
                 text = "차단할 앱 선택하기"
+            )
+            Image(
+                modifier= Modifier.size(24.dp),
+                painter = painterResource(ResourceR.drawable.ic_arrow_right),
+                colorFilter = ColorFilter.tint(color = Color.White),
+                contentDescription = null
             )
         }
     }
